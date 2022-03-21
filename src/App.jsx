5a5826e317ccb2c6aa2254c2fc382f98152d1ae9
kunida <67056950+kunida@users.noreply.github.com>
2022-03-21 11:15:13 +0900

    タスクの追加、削除

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,47 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["テスト", "テスト2"]);
   const [completeTodos, setCompleteTodos] = useState(["テスト"]);
+
+  //テキストボックスの入力できるように
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  const onClickDlete = (index) => {
+    //引数を渡すときは下のhtmlでアロー関数の形にする
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1); //splice 一つ目の引数で配列指定　2つ目でそこから何個を削除するか
+    setIncompleteTodos(newTodos);
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <li>
                 <div key={todo} className="list-row">
                   <p>{todo}</p>
                   <button>完了</button>
-                  <button>削除</button>
+                  <button onClick={() => onClickDlete(index)}>削除</button>
                 </div>
               </li>
             );
