564475c52638fe5668228e56f8ab96cada44260d
kunida <67056950+kunida@users.noreply.github.com>
2022-03-21 10:40:15 +0900

    html,css部分作製

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,42 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="input-area">
+        <input plavceholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>テスト</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li>
+            <div className="list-row">
+              <p>テスト2</p>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
+        <ul>
+          <li>
+            <div className="list-row">
+              <p>テスト</p>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
