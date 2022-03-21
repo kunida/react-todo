0bf59e71e5921df4c0a5fb3c20c1b8674934b9db
kunida <67056950+kunida@users.noreply.github.com>
2022-03-21 11:28:07 +0900

    完了、戻す機能追加

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,30 @@ export const App = () => {
     setTodoText("");
   };
 
+  //引数を渡すときは下のhtmlでアロー関数の形にする
   const onClickDlete = (index) => {
     //引数を渡すときは下のhtmlでアロー関数の形にする
     const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1); //splice 一つ目の引数で配列指定　2つ目でそこから何個を削除するか
     setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
   };
 
   return (
@@ -41,7 +60,7 @@ export const App = () => {
               <li>
                 <div key={todo} className="list-row">
                   <p>{todo}</p>
-                  <button>完了</button>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
                   <button onClick={() => onClickDlete(index)}>削除</button>
                 </div>
               </li>
@@ -52,12 +71,12 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了したTODO</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <li>
                 <div key={todo} className="list-row">
                   <p>{todo}</p>
-                  <button>戻す</button>
+                  <button onClick={() => onClickBack(index)}>戻す</button>
                 </div>
               </li>
             );
